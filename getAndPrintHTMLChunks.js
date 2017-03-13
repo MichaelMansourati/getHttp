022d25dee8dec220a410ff0301784245031c74ee
Michael Mansourati <mmansourati@gmail.com>
2017-03-13 18:29:15 +0000

    added http.get function

--- getAndPrintHTMLChunks.js
+++ getAndPrintHTMLChunks.js
@@ -5,6 +5,18 @@ function getAndPrintHTMLChunks () {
     path: '/http-examples/step1.html'
   };
 
-  /* Add your code here */
+  https.get(requestOptions, function (response) {
+
+    response.setEncoding('utf8');
+
+    response.on('data', function (data) {
+      console.log('Chunk Received. Length:', data.length);
+    });
+
+    response.on('end', function() {
+      console.log('Response stream complete.');
+    });
+
+  });
 
 }
