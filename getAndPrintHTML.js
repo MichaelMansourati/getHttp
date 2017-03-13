f84a737cacf5ba9265b33700ce8f7babdadfccf3
Michael Mansourati <mmansourati@gmail.com>
2017-03-13 18:45:57 +0000

    step two completed

--- getAndPrintHTML.js
+++ getAndPrintHTML.js
@@ -5,7 +5,26 @@ function getAndPrintHTML () {
     path: '/http-examples/step2.html'
   };
 
-  /* Add your code here */
+  var https = require('https');
+
+  https.get(requestOptions, function (response) {
+
+    response.setEncoding('utf8');
+
+    response.on('data', function (data) {
+      var allTheData = ''
+      for (i = 0; i < data.length; i++) {
+        allTheData += data[i];
+      }
+      console.log(allTheData);
+    });
+
+    response.on('end', function() {
+      console.log('Response stream complete.');
+    });
+
+  });
 
 }
 
+getAndPrintHTML ();
